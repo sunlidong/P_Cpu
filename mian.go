42868d23fc75d6271b602487398010744768e9c8
sld <654460176@qq.com>
2019-11-18 14:01:20 +0800

    3

---
+++ mian.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "fmt"
+    "github.com/shirou/gopsutil/cpu"
+    "github.com/shirou/gopsutil/disk"
+    "github.com/shirou/gopsutil/host"
+    "github.com/shirou/gopsutil/mem"
+)
+
+func main() {
+    v, _ := mem.VirtualMemory()
+    c, _ := cpu.CPUInfo()
+    d, _ := disk.DiskUsage("/")
+    n, _ := host.HostInfo()
+
+    fmt.Printf("        Mem       : %v GB  Free: %v MB Usage:%f%%\n", v.Total/4/1024/1024/1024/1024, v.Free/4/1024/1024/1024, v.UsedPercent)
+    if len(c) > 1 {
+        for _, sub_cpu := range c {
+            modelname := sub_cpu.ModelName
+            cores := sub_cpu.Cores
+            fmt.Printf("        CPU       : %v   %v cores \n", modelname, cores)
+        }
+    } else {
+        sub_cpu := c[0]
+        modelname := sub_cpu.ModelName
+        cores := sub_cpu.Cores
+        fmt.Printf("        CPU       : %v   %v cores \n", modelname, cores)
+
+    }
+    fmt.Printf("        HD        : %v GB  Free: %v GB Usage:%f%%\n", d.Total/1024/1024/1024, d.Free/1024/1024/1024, d.UsedPercent)
+    fmt.Printf("        OS        : %v   %v  \n", n.OS, n.PlatformVersion)
+    fmt.Printf("        Hostname  : %v  \n", n.Hostname)
+
+}
--- test1.go
+++ test1.go
@@ -7,6 +7,8 @@ import (
     "os/exec"
     "strconv"
     "strings"
+    "syscall"
+    "runtime"
 )
 
 type Process struct {
@@ -16,7 +18,7 @@ type Process struct {
 
 
  var num float64
-func main() {
+func main1() {
     cmd := exec.Command("ps", "aux")
     var out bytes.Buffer
     cmd.Stdout = &out
@@ -57,4 +59,34 @@ func main() {
 
     //
     log.Println("=====================================>",num)
+}
+
+import (
+
+)
+
+type MemStatus struct {
+    All  uint32 `json:"all"`
+    Used uint32 `json:"used"`
+    Free uint32 `json:"free"`
+    Self uint64 `json:"self"`
+}
+
+func MemStat() MemStatus {
+    //自身占用
+    memStat := new(runtime.MemStats)
+    runtime.ReadMemStats(memStat)
+    mem := MemStatus{}
+    mem.Self = memStat.Alloc
+
+    //系统占用,仅linux/mac下有效
+    //system memory usage
+    sysInfo := new(syscall.Sysinfo_t)
+    err := syscall.Sysinfo(sysInfo)
+    if err == nil {
+        mem.All = sysInfo.Totalram * uint32(syscall.Getpagesize())
+        mem.Free = sysInfo.Freeram * uint32(syscall.Getpagesize())
+        mem.Used = mem.All - mem.Free
+    }
+    return mem
 }
